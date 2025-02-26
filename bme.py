8ee9a762e31a1a42eb3310153d5a941a7428fbba
Robert R. Pace <robert.pace@eku.edu>
2025-02-26 04:43:04 -0500

    Add files via upload
    Added check for existing CSV, to prevent redundant headers

--- bme.py
+++ bme.py
@@ -1,12 +1,22 @@
 from datetime import datetime
 import time, smbus2, bme280, os
+
 # Fetch hostname
 host = os.uname()[1]
 
 # Initialize with current date/time, hostname, paths, and build logger filename
 now = datetime.now()
-ts = now.strftime("%Y-%m-%d %H:%M:%S")
-filename = "/bme280/data/sd_" + host + "_" + now.strftime("%Y-%m-%d_%Hh%Mm%Ss") + ".csv"
+ts = now.strftime("%Y-%m-%d %H:%M")
+filename = "/bme280/data/" + host + ".csv"
+file_path = filename
+
+# Check to see if a pre-existing csv exists, if so skip adding an additional header
+if os.path.exists(file_path):
+     print('')
+else:
+     f = open(filename, 'a+')
+     f.write('ts,temp,humidity,pressure\n');
+     f.close
 
 # BME280 sensor address (default address)
 address = 0x76
@@ -17,38 +27,43 @@ bus = smbus2.SMBus(2)
 # Load calibration parameters
 calibration_params = bme280.load_calibration_params(bus, address)
 
-# Create data file with column headers
-f = open(filename, 'a+')
-f.write('ts, temp, humidity, pressure\n');
-f.close
-
 while True:
     try:
+    	  # Construct time stamp
         now = datetime.now()
-        ts = now.strftime("%Y-%m-%d %H:%M:%S")
+        ts = now.strftime("%Y-%m-%d %H:%M")
+        	
         # Read sensor data
         data = bme280.sample(bus, address, calibration_params)
-        # Extract temperature, pressure, and humidity
-        temperature = round(data.temperature,1)
-        temperature = str(temperature)
-        pressure = round(data.pressure,1)
-        pressure = str(pressure)
-        humidity = round(data.humidity,1)
-
-        # Build output data string
-        data = ts, temperature, humidity, pressure
+        
+        # Extract temperature, humidity, and pressure from bme280 sensor
+        # Convert and round off temperature, humidity, and pressure from integers to strings
+        temperature = str(round(data.temperature))
+        humidity = str(round(data.humidity))
+        pressure = str(round(data.pressure))
+        
+        #build a  data string that will be output to the csv file
+        data = ts + "," + temperature + "," + humidity + "," + pressure 
+        
+        # Ensure that output data to CSV is a string
         output = str(data)
-        # Remove extraneous parentheses
+        
+        # Remove extraneous element chars such as open/close parenthesis and single quotes
         output = output.replace("(","")
         output = output.replace(")","")
-
-        # Write data to sensor_data.csv
+        output = output.replace("'","")
+        
+        # Write data to CSV
+        # Open csv file as append/create
         f = open(filename, 'a+')
         f.write(output + "\n")
         f.close()
+        
+        # Clear some variables
         del now, ts
+        
         # Wait for a few seconds before the next reading
-        time.sleep(60)
+        time.sleep(300)
 
     except KeyboardInterrupt:
         print('Program stopped')
